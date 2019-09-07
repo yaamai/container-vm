616970e97236cbe5f38df8839b8e9f06c0026c3b
aina <aina@airiss.tk>
2019-09-08 03:24:36 +0900

    Support multiple host up

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,24 @@
 #!/bin/bash -x
 
 BASE_IMAGE_FILE=$(readlink -f $1)
+NUM_OF_VM=${2:-1}
 
 function main() {
-  DATA_DIR=${DATA_DIR_BASE:-$PWD}/$HOSTNAME
-  mkdir -p $DATA_DIR
 
-  prepare_cloud_config $DATA_DIR/cloud-config.iso
-  prepare_image $DATA_DIR/instance.qcow2 $BASE_IMAGE_FILE
-  launch_vm $HOSTNAME $DATA_DIR/instance.qcow2 $DATA_DIR/cloud-config.iso
+  for ((num=1; num<$((NUM_OF_VM+1)); num++)); do
+    name=$(printf "%s-%02d" $HOSTNAME $num)
+    data_dir=${DATA_DIR_BASE:-$PWD}/$name
+    mkdir -p $data_dir
+
+    prepare_cloud_config $data_dir/cloud-config.iso
+    prepare_image $data_dir/instance.qcow2 $BASE_IMAGE_FILE
+    launch_vm \
+      $name \
+      $data_dir/instance.qcow2 \
+      $data_dir/cloud-config.iso \
+      $(printf "02:42:ac:11:00:%02d" $num) \
+      $(printf "192.168.122.%s" $num)
+  done
 }
 
 function prepare_cloud_config() {
@@ -49,11 +59,12 @@ function launch_vm() {
   local name=$1
   local image=$2
   local config_image=$3
-  local ram=${4:-1024}
-  local cpu=${5:-1}
-  local mac=${6:-"02:42:ac:11:00:04"}
-  local ip=${7:-"192.168.122.10"}
+  local mac=${4:-"02:42:ac:11:00:04"}
+  local ip=${5:-"192.168.122.10"}
+  local ram=${6:-1024}
+  local cpu=${7:-1}
 
+  virsh net-update default delete ip-dhcp-host "<host mac='$mac' ip='$ip'/>" --live --config || true
   virsh net-update default add-last ip-dhcp-host "<host mac='$mac' ip='$ip'/>" --live --config
 
   virt-install \
