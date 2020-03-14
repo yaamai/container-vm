55ff3b4f4984a8550cebd3a8064a2608c25b3c53
aina <aina@airiss.tk>
2020-03-15 00:38:56 +0900

    Fix linter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,67 +1,66 @@
 #!/bin/ash -x
 
-BASE_IMAGE_FILE=$(readlink -f $1)
+BASE_IMAGE_FILE=$(readlink -f "$1")
 MEMORY=${2:-512}
 CPU=${3:-2}
 CLOUD_CONFIG=${4:-""}
+HOSTNAME=$(hostname -s)
 
-function main() {
-  name=$(printf "%s" $HOSTNAME)
+main() {
+  name=$(printf "%s" "$HOSTNAME")
   data_dir=${DATA_DIR_BASE:-$PWD/vms}/$name
-  mkdir -p $data_dir
+  mkdir -p "$data_dir"
 
-  prepare_cloud_config $data_dir/cloud-config.iso $CLOUD_CONFIG
-  if ! check_file_exists $data_dir/cloud-config.iso; then
-    prepare_cloud_config $data_dir/cloud-config.iso $CLOUD_CONFIG
-  fi
+  prepare_cloud_config "$data_dir/cloud-config.iso" "$CLOUD_CONFIG"
 
-  if ! check_file_exists $data_dir/instance.qcow2; then
-    prepare_image $data_dir/instance.qcow2 $BASE_IMAGE_FILE
+  if ! check_file_exists "$data_dir/instance.qcow2"; then
+    prepare_image "$data_dir/instance.qcow2" "$BASE_IMAGE_FILE"
   fi
 
   qemu-system-x86_64 \
     --enable-kvm \
     -object iothread,id=io1 \
     -device virtio-blk-pci,drive=disk0,iothread=io1 \
-    -drive if=none,id=disk0,cache=none,format=qcow2,aio=threads,file=$data_dir/instance.qcow2 \
-    -cdrom $data_dir/cloud-config.iso \
-    -nic user,hostfwd=tcp::22-:22,smb=$PWD \
+    -drive "if=none,id=disk0,cache=none,format=qcow2,aio=threads,file=$data_dir/instance.qcow2" \
+    -cdrom "$data_dir/cloud-config.iso" \
+    -nic "user,hostfwd=tcp::22-:22,smb=$PWD" \
     -display vnc=:0 \
-    -m $MEMORY \
-    -smp $CPU \
+    -m "$MEMORY" \
+    -smp "$CPU" \
     -daemonize \
-    -serial pty 2>&1 | tee /tmp/qemu-$name-stdout.log
+    -serial pty 2>&1 | tee "/tmp/qemu-$name-stdout.log"
 
   while true; do
-    minicom -p $(cat /tmp/qemu-$name-stdout.log | grep -Eo "/dev/pts/[0-9]+")
-    if [[ $? -ne 0 ]]; then
+    pty="$(grep -Eo '/dev/pts/[0-9]+' < "/tmp/qemu-$name-stdout.log")"
+    if minicom -p "$pty"; then
       break
     fi
   done
 }
 
-function check_file_exists() {
+check_file_exists() {
   local path=$1
-  if [[ -e $path ]]; then
+  if [ -e "$path" ]; then
     true; return $?
   fi
 
   false; return $?
 }
 
-function prepare_cloud_config() {
+prepare_cloud_config() {
   local path=$1
   local cloud_config=$2
-  local base_path=$(dirname $path)
+  local base_path
+  base_path="$(dirname "$path")"
 
-  cat >$base_path/meta-data <<EOF
+  cat > "$base_path/meta-data" <<EOF
 local-hostname: $HOSTNAME
 EOF
 
-  if [[ ! -z $cloud_config ]]; then
-    cp -f $cloud_config $base_path/user-data
+  if [ -n "$cloud_config" ]; then
+    cp -f "$cloud_config" "$base_path/user-data"
   else
-    cat >$base_path/user-data <<EOF
+    cat > "$base_path/user-data" <<EOF
 #cloud-config
 
 password: Passw0rd1234
@@ -72,11 +71,11 @@ EOF
   fi
 
   genisoimage \
-    -output $path \
-    -volid cidata -joliet -rock $base_path/user-data $base_path/meta-data
+    -output "$path" \
+    -volid cidata -joliet -rock "$base_path/user-data" "$base_path/meta-data"
 }
 
-function prepare_image() {
+prepare_image() {
   local path=$1
   local image=$2
   local size=${3:-${VM_DISK_SIZE:-16G}}
@@ -84,9 +83,9 @@ function prepare_image() {
   qemu-img create \
     -f qcow2 \
     -o cluster_size=2M \
-    -o backing_file=$image \
-    $path \
-    $size
+    -o "backing_file=$image" \
+    "$path" \
+    "$size"
 }
 
 main "$@"
